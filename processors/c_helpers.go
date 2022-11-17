6e5d4ae7740e31141008eedfc73cc5e535f83331
Rich Baird <rich@rbaird.me>
2022-11-16 22:33:54 -0700

    1 << 27

--- processors/c_helpers.go
+++ processors/c_helpers.go
@@ -9,12 +9,13 @@ func sliceToCArray[T, V any](slice []T, to V) unsafe.Pointer {
 	count := C.int(len(slice))
 	cArray := C.malloc(C.size_t(count) * C.size_t(unsafe.Sizeof(to)))
 	// convert the C array to a Go Array so we can index it
-	a := (*[count * unsafe.sizeof(to)]V)(cArray)
+	a := (*[1<<27 - 1]V)(cArray)
 	// fill the C array with values
 	for index, value := range slice {
 		// the following code coerces value to an interface compatible with V
 		// where V is a C type compatible with go type T
 		v := *((*V)((unsafe.Pointer(&value))))
+		if(index == len(slice)) { break }
 		a[index] = V(v)
 	}
 
